a6508bad4bc2ea9bc48b78e0f65060e8f931f4cb
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-12 22:21:20 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
-# Write your code here
+class GetRequester
+ 
+ 
+def initialize(url)
+ URL
+end 
+  
+def get_response_body  
+  uri = URI.parse(URL)
+  response = Net::HTTP.get_response(uri)
+  response.body
+end 
+
+def parse_json 
+  
+end  
+
+end
+
